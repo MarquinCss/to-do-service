8e932cb5c68f4c1186d5b15d52d76d6454baadef
Marco Marchi <antoniomarchi2006@gmail.com>
2023-11-05 21:50:43 -0300

    Criando rep

---
+++ src/main/java/com/example/todoservice/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.example.todoservice.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String printHelloWorld(){
+        return "um frontender que rodou um controller em spring, é isso mesmo???";
+    }
+}
